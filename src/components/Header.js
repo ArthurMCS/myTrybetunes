ec87fd9b7125d0812ab99d7977c4d024b8400ae8
Arthur Moreira <arthurmcs96@gmail.com>
2021-12-25 13:30:54 -0300

    finaliza o Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
+
+const HeaderStyled = styled.header`
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      background: #036b52;
+      height: 70px;
+
+      h2 {
+        margin-top: 10px;
+        font-size: 30px; 
+      }
+
+      .link {
+        color: black;
+        font-size: 30px;
+        margin-right: 20px;
+        transition: all 0.6s ease;
+      }
+
+      .link:hover {
+        color: #fff;
+      }
+`;
 
 export default function Header() {
   const [user, setUser] = useState('');
@@ -23,30 +48,36 @@ export default function Header() {
       {isLoading
         ? <Loading />
         : (
-          <>
-            <p>TrybeTunes</p>
+          <HeaderStyled>
+            <h2>TrybeTunes</h2>
             <nav>
               <Link
                 data-testid="link-to-search"
                 to="/search"
+                style={ { textDecoration: 'none' } }
+                className="link"
               >
                 Pesquisar
               </Link>
               <Link
                 data-testid="link-to-favorites"
                 to="/favorites"
+                style={ { textDecoration: 'none' } }
+                className="link"
               >
                 Músicas Favoritas
               </Link>
               <Link
                 data-testid="link-to-profile"
                 to="/profile"
+                style={ { textDecoration: 'none' } }
+                className="link"
               >
                 Perfil
               </Link>
             </nav>
-            <p data-testid="header-user-name">{user.name}</p>
-          </>
+            <h2 data-testid="header-user-name">{user.name}</h2>
+          </HeaderStyled>
         )}
     </header>
   );
--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+// https://www.youtube.com/watch?v=zWmaohjrkRw&t=1111s
+const LoadingStyled = styled.div`
+      width: 50px;
+      height: 50px;
+      border: 10px solid green;
+      border-top-color: transparent;
+      border-radius: 50%;
+      margin: 0 auto;
+      animation: loading 0.6s  linear infinite ;
+
+  
+      @keyframes loading { 
+        to { 
+          transform: rotate(360deg);
+        }
+      }
+`;
 
 export default class Loading extends Component {
   render() {
     return (
       <div>
+        <LoadingStyled />
         Carregando...
       </div>
     );
